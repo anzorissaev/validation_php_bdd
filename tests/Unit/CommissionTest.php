216089cb7b98593b21b08315a0b326c99830313a
Laurent Beauvisage <laurentbeauvisage@gmail.com>
2018-05-07 16:46:41 +0200

    ADD test for Commission Amount Getter

--- tests/Unit/CommissionTest.php
+++ tests/Unit/CommissionTest.php
@@ -29,5 +29,27 @@ class CommissionTest extends TestCase
         $this->assertEquals($expected, $actual);
     }
 
- 
+    /**
+     * Test de la commission perçue par le site.
+     *
+     * @return void
+     */
+    /* Code a décommenter
+    public function testCommissionAmountGetter()
+    {
+        // Etant donné que la commission est de 10%
+        $donation = new Commission(10);
+
+        // Lorsque le site reçoit une donation de 100
+        $donation->setDonation(100);
+
+        // Alors la Valeur de la commission doit être de 10
+        $expected = 10;
+        $actual = $donation->getCommissionAmount();
+
+        $this->assertEquals($expected, $actual);
+    }
+    */
+
+
 }
