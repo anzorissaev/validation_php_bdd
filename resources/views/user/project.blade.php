6f4989c28138cb51f97437044e96ca1cb29702e9
anzorissaev <e.issaev@gmail.com>
2018-08-14 14:28:06 +0200

    Creation Model Projet et Project Controller Vue pour la pages des projet des utilisateur

--- resources/views/user/project.blade.php
+++ resources/views/user/project.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
 
-   <div>Mes projets</div>
-   <div>Mes projets</div>
-   <div>Mes projets</div>
-   <div>Mes projets</div>
-   <div>Mes projets</div>
+   <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-md-8">
+            <div class="card">
+               <div class="justify-content-between">
+                  <div class="card-header">Mes Projets</div>
 
+                  <div class="card-body">1</div>
+                  <div class="card-body">2</div>
+
+               </div>
+            </div>
+         </div>
+          <div class="col-md-8">
+              <div class="mt-3"><button type="button" class="btn btn-success">Crée un projet</button></div>
+          </div>
+      </div>
+   </div>
 
 @endsection
