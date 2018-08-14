ed0820ccb19aa9107ee1feb812d7de34a3a5501a
anzorissaev <e.issaev@gmail.com>
2018-08-14 11:15:36 +0200

    Créer un Controller “AccountController” qui contiendra les “methods” nécessaire à la mise en jour du profils utilisateur. Un utilisateur ne peut mettre à jour que son profil. Un utilisateur qui n’est pas identifié ne peux pas accéder à ces pages.

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    public function edit()
+
+    {
+        $user = Auth::user();
+        return view('user.edit',['user' => $user]);
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        $validateData = $request->validate([
+            'lastname'=> 'required|alpha|string|max:45|min:2',
+            'firstname'=> 'required|alpha|string|max:45|min:2',
+            'email'=> 'string|required|email|max:255|unique:users,email,'.$user->id,
+        ]);
+        $user->update($validateData);
+
+        return view('user.edit',['user' => $user]);
+    }
+}
